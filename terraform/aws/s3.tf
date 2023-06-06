33a6f6fafb1bff114cbf67af9ed787676df52414
Pavel Kerbel <kerbelp@gmail.com>
2023-06-06 10:21:31 +0300

    add test bucket to s3.tf

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -139,3 +139,7 @@ resource "aws_s3_bucket" "logs" {
     yor_trace            = "01946fe9-aae2-4c99-a975-e9b0d3a4696c"
   })
 }
+
+resource "aws_s3_bucket" "test" {
+    bucket = "${local.resource_prefix.value}-test"
+}
