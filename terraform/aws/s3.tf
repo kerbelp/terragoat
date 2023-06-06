7226c6065a0bd2472aca7b586d96f53f2de35a1f
Pavel Kerbel <kerbelp@gmail.com>
2023-06-06 15:33:10 +0300

    Update s3.tf

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -147,3 +147,7 @@ resource "aws_s3_bucket" "test" {
 resource "aws_s3_bucket" "test2" {
     bucket = "${local.resource_prefix.value}-test2"
 }
+
+resource "aws_s3_bucket" "test3" {
+    bucket = "${local.resource_prefix.value}-test3"
+}
